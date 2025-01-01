d1ed979c1c39f51fa718480d7dbbdfce9fe7f13d
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-01-01 17:23:03 -0300

    botoes baixar

--- web/user/src/components/os/index.jsx
+++ web/user/src/components/os/index.jsx
@@ -1,10 +1,35 @@
+import { toast } from "react-toastify";
 import "./style.css";
 
 export default function Os({ img, title, url }) {
-  console.log(url);
+  function handleClick(e, link) {
+    console.log(!link);
+
+    e.preventDefault();
+    e.stopPropagation();
+    try {
+      if (!link) {
+        throw new Error("Em breve teremos esta funcionalidade");
+      } else {
+        toast.info("Redirecionando...", {
+          position: "top-left",
+        });
+      }
+    } catch (error) {
+      toast.error(error.message, {
+        position: "top-center",
+      });
+    }
+  }
+
   return (
-    <div className={`os ${!url && "relative"}`}>
-      <h2 className={!url ? "emBreve" : "existe"}>EM BREVE</h2>
+    <div
+      className={`os ${!url && "relative"}`}
+      onClick={(e) => handleClick(e, url)}
+    >
+      <h2 className={!url ? "emBreve" : "existe"}>
+        {!url ? "EM BREVE" : title}
+      </h2>
       <img src={img} alt={title} />
     </div>
   );
