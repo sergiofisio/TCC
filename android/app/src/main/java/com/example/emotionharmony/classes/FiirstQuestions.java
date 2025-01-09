428880ca03eb458dcdc2841f546a613f84ace1ef
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-01-09 12:38:49 -0300

    primeiras telas prontas

---
+++ android/app/src/main/java/com/example/emotionharmony/classes/FiirstQuestions.java
@@ -0,0 +1,37 @@
+package com.example.emotionharmony.classes;
+
+public class FiirstQuestions {
+    public String question1, question2, emotion, description;
+
+    public String getQuestion1() {
+        return question1;
+    }
+
+    public void setQuestion1(String question1) {
+        this.question1 = question1;
+    }
+
+    public String getQuestion2() {
+        return question2;
+    }
+
+    public void setQuestion2(String question2) {
+        this.question2 = question2;
+    }
+
+    public String getEmotion() {
+        return emotion;
+    }
+
+    public void setEmotion(String emotion) {
+        this.emotion = emotion;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
