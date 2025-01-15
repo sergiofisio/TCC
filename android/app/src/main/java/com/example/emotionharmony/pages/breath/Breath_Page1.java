4eebcbcb3eaa19b59d779aa147dfcacf037b710c
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-01-15 09:38:37 -0300

    respirações

---
+++ android/app/src/main/java/com/example/emotionharmony/pages/breath/Breath_Page1.java
@@ -0,0 +1,29 @@
+package com.example.emotionharmony.pages.breath;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.emotionharmony.R;
+import com.example.emotionharmony.databinding.ActivityBreathPage1Binding;
+
+public class Breath_Page1 extends AppCompatActivity {
+
+    private ActivityBreathPage1Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_breath_page1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- android/app/src/main/java/com/example/emotionharmony/pages/meditation/After_Login_Page1.java
+++ android/app/src/main/java/com/example/emotionharmony/pages/meditation/After_Login_Page1.java
@@ -3,6 +3,7 @@ package com.example.emotionharmony.pages.meditation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.emotionharmony.R;
+import com.example.emotionharmony.pages.After_Login;
 
 public class After_Login_Page1 extends AppCompatActivity {
 
@@ -27,6 +29,7 @@ public class After_Login_Page1 extends AppCompatActivity {
         });
 
         ImageView btnNext1 = findViewById(R.id.btnNext1);
+        Button btnSair = findViewById(R.id.btnSair);
 
         btnNext1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +39,13 @@ public class After_Login_Page1 extends AppCompatActivity {
 
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
+        });
+
+        btnSair.setOnClickListener(V ->{
+            Intent intent = new Intent(After_Login_Page1.this, After_Login.class);
+            startActivity(intent);
+
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         });
     }
 }
