e24a1cbf85043f7920ea2a19d192e37b9a0e68fd
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-02-04 08:14:09 -0300

    novo login

--- android/app/src/main/java/com/example/emotionharmony/pages/breath/Breath_Page1.java
+++ android/app/src/main/java/com/example/emotionharmony/pages/breath/Breath_Page1.java
@@ -1,13 +1,16 @@
 package com.example.emotionharmony.pages.breath;
 
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.emotionharmony.R;
@@ -16,33 +19,55 @@ import com.example.emotionharmony.databinding.ActivityBreathPage1Binding;
 
 public class Breath_Page1 extends AppCompatActivity {
 
-    private TextView timer;
+    private TextView timer, breathingInstruction;
     private CountDownTimer countDownTimer;
     private long timeLeftIntMillis = 600000;
+    private LinearLayout instructions, Breath, modalEnd;
     private ActivityBreathPage1Binding binding;
     private BreathingCircleView breathCircle;
     private Handler handler = new Handler(Looper.getMainLooper());
-    private int minute = 1;
-    private int rpm = 12;
+    private int minute = 1,  rpm = 12;
     private ValueAnimator animator;
+    private Button btnBegin, btnSim, btnNao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         binding = ActivityBreathPage1Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         breathCircle = findViewById(R.id.breathingCircleView);
         timer = findViewById(R.id.timer);
+        breathingInstruction = findViewById(R.id.breathingInstruction);
+        btnBegin = findViewById(R.id.btnBegin);
+        btnSim = findViewById(R.id.btnSim);
+        btnNao = findViewById(R.id.btnNao);
 
-        startBreathingExercise();
+        btnBegin.setOnClickListener(v->{
+
+            startBreathingExercise();
+        });
+
+
     }
 
     private void startBreathingExercise(){
-        updateRPM();
-        startBreathCycle();
-        startTimer();
+        instructions.setVisibility(View.GONE);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            runOnUiThread(() -> {
+                updateRPM();
+                startBreathCycle();
+                startTimer();
+            });
+        }).start();
+
     }
 
     private void startTimer() {
@@ -92,6 +117,14 @@ public class Breath_Page1 extends AppCompatActivity {
         animator.addUpdateListener(animation ->{
             float progress = (float) animation.getAnimatedValue();
             breathCircle.setProgress(progress);
+
+            if (progress >= 359 ) {
+                breathingInstruction.setText("EXPIRE");
+                animateTextSize(breathingInstruction, 2f, 1f, cycleDuration);
+            } else if(progress<=3 ) {
+                breathingInstruction.setText("INSPIRE");
+                animateTextSize(breathingInstruction, 1f, 2f, cycleDuration);
+            }
         });
 
         animator.start();
@@ -105,7 +138,18 @@ public class Breath_Page1 extends AppCompatActivity {
 
     private void stopBreathAnimation() {
         if (animator != null && animator.isRunning()) {
-            animator.cancel(); // Cancela a animação
+            animator.cancel();
         }
+    }
+
+    private void animateTextSize(TextView textView, float startScale, float endScale, long duration) {
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(textView, "scaleX", startScale, endScale);
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(textView, "scaleY", startScale, endScale);
+
+        scaleX.setDuration(duration);
+        scaleY.setDuration(duration);
+
+        scaleX.start();
+        scaleY.start();
     }
 }
