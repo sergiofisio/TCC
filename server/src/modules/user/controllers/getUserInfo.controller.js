0a0efc9330b40fe8fbf39348ed15f5f3d5110085
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-05-02 10:31:46 -0300

    new server

--- server/src/modules/user/controllers/getUserInfo.controller.js
+++ server/src/modules/user/controllers/getUserInfo.controller.js
@@ -1,9 +1,17 @@
+const CustomError = require("../../../core/errors/CustomError");
 const { getUserInfoService } = require("../services/getUserInfo.service");
 
 module.exports = async (req, res, next) => {
   try {
-    const id_user = req.params.id ? Number(req.params.id) : req.user.id_user;
-    const result = await getUserInfoService(id_user);
+    const isAdmin = req.user.type_user === "admin";
+    const requestedId = Number(req.params.id);
+    const ownId = req.user.id_user;
+
+    if (!isAdmin && requestedId && requestedId !== ownId)
+      throw new CustomError("Acesso não autorizado", 403);
+
+    const idToFetch = requestedId || ownId;
+    const result = await getUserInfoService(idToFetch);
     res.status(200).json(result);
   } catch (err) {
     next(err);
